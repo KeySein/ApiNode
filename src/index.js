b4865116d1ff45be46ea7528c04384c0605aea9f
KeySein <k.sein.t971@edu.utc.mx>
2025-03-28 10:11:16 -0600

    actualizado

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
+// Importación de módulos
+
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+
 const cors = require('cors');
 
-// Configuraciones del servidor
 app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
+app.set('json spaces', 2); 
 
 // Middleware
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-//app.use(express.json());
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json());
 
-// Endpoint para sumar dos números
-app.post('/sumar', (req, res) => {
-  const { num1, num2 } = req.body;
-
-  // Validar que se hayan enviado los dos números y que no estén vacíos
-  if (!num1 || !num2) {
-    return res.status(400).send({ error: 'Faltan números para sumar' });
-  }
-
-  // Sumar los números
-  const resultado = parseFloat(num1) + parseFloat(num2);
-
-  // Enviar el resultado
-  res.send({ resultado });
+app.use (cors());
+// Nuestro primer web service
+app.get('/', (req, res) => {
+    res.json({
+        "mensaje": "Mi primer pag"
+    }
+  );
 });
+
+app.post('/sumar', (req, res) => { //http://localhost/sumar
+    const { num1, num2 } = req.body; // El req.body está esperando los números
+
+    // Validar que se hayan enviado los dos números y que no estén vacíos
+    if (num1 === undefined || num2 === undefined) {
+        return res.status(400).send({ error: 'Faltan números para sumar' });
+    }
+
+    // Sumar los números (asegurando que sean convertidos a número)
+    const resultado =  num1 + num2 ;
+
+    // Enviar el resultado
+    res.send({ resultado });
+});
+
 
 // Iniciando el servidor
 app.listen(app.get('port'), () => {
-  console.log(`Server listening on port ${app.get('port')}`);
+    console.log(`Server listening on port ${app.get('port')}`);
 });
